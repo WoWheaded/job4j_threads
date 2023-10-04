67c6c920670918492eac0310f52cf5b845c20566
Axiom <d-343@yandex.ru>
2023-10-04 17:37:20 +0300

    1. Реализовать ThreadPool [#1099]

---
+++ src/main/java/ru/job4j/pool/ThreadPool.java
@@ -0,0 +1,48 @@
+package ru.job4j.pool;
+
+import ru.job4j.SimpleBlockingQueue;
+
+import java.util.LinkedList;
+import java.util.List;
+
+public class ThreadPool {
+    private final int size = Runtime.getRuntime().availableProcessors();
+    private final List<Thread> threads = new LinkedList<>();
+    private final SimpleBlockingQueue<Runnable> tasks = new SimpleBlockingQueue<>(size);
+
+    public ThreadPool() {
+        for (int i = 0; i <= size; i++) {
+            Thread thread = new Thread(() -> {
+                while (!Thread.currentThread().isInterrupted()) {
+                    try {
+                        tasks.poll().run();
+                    } catch (InterruptedException e) {
+                        Thread.currentThread().interrupt();
+                    }
+                }
+            });
+            thread.start();
+            threads.add(thread);
+        }
+    }
+
+    public void work(Runnable job) {
+        try {
+            tasks.offer(job);
+        } catch (InterruptedException e) {
+            throw new RuntimeException(e);
+        }
+    }
+
+    public void shutdown() {
+        threads.forEach(Thread::interrupt);
+    }
+
+    public static void main(String[] args) {
+        ThreadPool threadPool = new ThreadPool();
+        for (int i = 0; i < threadPool.size; i++) {
+            threadPool.work(() -> System.out.println(Thread.currentThread().getName()));
+        }
+        threadPool.shutdown();
+    }
+}
